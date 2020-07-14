229221a20496c2554d00bdc83775fff0cdcb8b42
Ivan Egorov <xomageimer@Gmail.com>
2020-07-14 22:07:51 +0300

    part2 hw1

---
+++ HW1/Part2/Build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/xomag/CLionProjects/OtusHomeWorks/HomeWorks/HW1/Part2
+# Build directory: C:/Users/xomag/CLionProjects/OtusHomeWorks/HomeWorks/HW1/Part2/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ip_print_tests "tester")
+set_tests_properties(ip_print_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/xomag/CLionProjects/OtusHomeWorks/HomeWorks/HW1/Part2/CMakeLists.txt;64;add_test;C:/Users/xomag/CLionProjects/OtusHomeWorks/HomeWorks/HW1/Part2/CMakeLists.txt;0;")
