3b27e7ed13d147044107c55d240818adff15de0d
Ivan Egorov <xomageimer@Gmail.com>
2020-11-03 10:47:25 +0300

    bulk fix argv

--- HW6/src/main.cpp
+++ HW6/src/main.cpp
@@ -1,7 +1,7 @@
 #include "Bulk.h"
 
 
-int main(int argv, [[maybe_unused]] char* argc[]){
+int main( [[maybe_unused]] int argc, char* argv[]){
     Bulk blk;
 
     blk.SetController(std::make_shared<BulkController>());
@@ -9,5 +9,5 @@ int main(int argv, [[maybe_unused]] char* argc[]){
     blk.SubscribeLogger("Console_Outputer", std::make_shared<ConsoleLogger>(std::cout));
     blk.SubscribeLogger("File_Outputer", std::make_shared<FileLogger>(std::filesystem::current_path()));
 
-    blk.run(argv);
+    blk.run(std::stoi(argv[1]));
 }
